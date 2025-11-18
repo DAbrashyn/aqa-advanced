c06088643c59a004a3c6f2b96edeb00dc6b61c35
Denys Abrashyn <denis.abrashin@gmail.com>
2025-11-18 15:42:02 +0200

    HW 5.1. JavaScript conditions and loops

---
+++ HW5/task3.js
@@ -0,0 +1,9 @@
+"use strict";
+//Завдання 3: Генерація таблиці множення -  Використовуючи цикл for
+
+const number = 6;
+
+for (let i = 1; i < 11; i++) {
+    let sum = number * i;
+  console.log(`${number} x ${i} = ${sum}`);
+}
